f604bcdfee0eeed6524d8f010957b45d20118107
Palash <palash.kanti.kundu.90@gmail.com>
2022-05-20 23:30:39 +0530

    Refactor test script

---
+++ emulator/src/test/simulatorTest.js
@@ -0,0 +1,23 @@
+const expect = require('chai').expect;
+const simulator = require('../simulator/simulator');
+const fs = require('fs');
+const path = require('path')
+const getInputOutput = (input) => {
+    var inputPath = path.join(__dirname, input + "_input.hdl");
+    var outputPath = path.join(__dirname, input + "_output.hdl");
+    return {
+        input: fs.readFileSync(inputPath, 'utf8'),
+        output: fs.readFileSync(outputPath, 'utf8')
+    }
+}
+
+describe("HDL Interpreter Test", function () {
+    describe("Remove comments", function () {
+        it("Removes multi-line comments", function () {
+            var io = getInputOutput("program1");
+            var result = simulator.simulate(io.input);
+            expect(io.output).to.equal(result);
+        }
+        );
+    });
+})
