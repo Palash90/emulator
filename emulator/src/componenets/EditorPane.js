cf611d4ffdc24b2a8d10f059dcaec277845334d3
Palash <palash.kanti.kundu.90@gmail.com>
2022-05-20 17:02:24 +0530

    basic editor added

--- emulator/src/componenets/EditorPane.js
+++ emulator/src/componenets/EditorPane.js
@@ -1,34 +1,48 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import FileContext from "./FileContext";
 import SplitPane from "react-split-pane";
+
+
+import Editor from "react-simple-code-editor";
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/themes/prism.css";
 
 export const EditorPane = (props) => {
     const { files, currFile } = useContext(FileContext);
     const file = files.find((el) => el.id === currFile);
 
-    var content = "default"
-
+    var content = "default";
     if (file) {
         if (file.content) {
-            content = file.content;
+            content = file.content
         } else {
             content = file.id + " -> " + file.name;
         }
     }
 
+    var [code, setCode] = useState(content);
     return (
-
         <SplitPane split="horizontal"
             minSize={400}
-            defaultSize={parseInt(localStorage.getItem('splitPosEditorPane')||"400", 10)}
+            defaultSize={parseInt(localStorage.getItem('splitPosEditorPane') || "400", 10)}
             onChange={(size) => localStorage.setItem('splitPosEditorPane', size)}>
             <div >
-                Editor Window
+                <Editor
+                    value={code}
+                    onValueChange={(code) => setCode(code)}
+                    highlight={(code) => highlight(code, languages.js)}
+                    padding={10}
+                    style={{
+                        fontFamily: '"Fira code", "Fira Mono", monospace',
+                        fontSize: 12,
+                    }}
+                />
             </div>
             <div>
                 Output Window
             </div>
         </SplitPane>
     );
-
 };
