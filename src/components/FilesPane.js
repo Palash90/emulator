3cbbae5bccdb4c4cc796b45f7c4a0066b486e5b4
Palash <palash.kanti.kundu.90@gmail.com>
2022-05-22 01:38:21 +0530

    some beautification

--- src/components/FilesPane.js
+++ src/components/FilesPane.js
@@ -1,50 +1,27 @@
 import NewFile from "./NewFile";
 import FileContext from "./FileContext";
 import { useContext } from "react";
-import SplitPane from "react-split-pane";
-import Button from 'react-bootstrap/Button';
-import runSimulation from "./hdlSimulator";
-import SimulationContext from "./SimulationContext";
-
 
 function FilesPane() {
-  const { files, currFile, setCurrFile, setFiles } = useContext(FileContext);
-  const { setSimulationResult } = useContext(SimulationContext);
+  const { files, currFile, setCurrFile } = useContext(FileContext);
 
   return (
-    <SplitPane split="horizontal"
-      minSize={45}
-      defaultSize={parseInt(localStorage.getItem('splitPosFilesPane') || "45")}
-      onChange={(size) => localStorage.setItem('splitPosFilesPane', size)}>
-
-      <div className="btn-group btn-group-sm" role="group" >
-        <Button className="btn-success" type="button" onClick={() => localStorage.setItem('files', JSON.stringify(files))}>Save</Button>
-        <Button type="button" onClick={() => runSimulation(currFile, files, (result) => setSimulationResult(result))}>Run</Button>
-        <Button className="btn-success" type="button" href={`data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(files))}`} download="project.json">Download</Button>
-        <Button className="btn-danger" type="button" onClick={() => deleteProject()}>Delete</Button>
-      </div>
-      <div  >
-        <h6>Files:</h6>
-        <ul>
-          {files.map((el, i) => {
-            return FileDisplay(i, currFile, setCurrFile, el);
-          })}
-        </ul>
-        <NewFile></NewFile>
-      </div>
-    </SplitPane>
+    <div  >
+      <h6>Files:</h6>
+      <ul>
+        {files.map((el, i) => {
+          return FileDisplay(i, currFile, setCurrFile, el);
+        })}
+      </ul>
+      <NewFile></NewFile>
+    </div>
   );
-
-  function deleteProject() {
-    setFiles([])
-    return localStorage.setItem('files', JSON.stringify([]));
-  }
 }
 
 function FileDisplay(i, currFile, setCurrFile, el) {
   if (el && el.name && el.name !== '') {
     return <a key={i} href="#" onClick={() => setCurrFile(el.key)}>
-      <li className={el.key == currFile?"selectedFile":"unselectedFile"}>
+      <li className={el.key == currFile ? "selectedFile" : "unselectedFile"}>
         {el.name}
       </li>
     </a>;
--- src/components/MainPane.js
+++ src/components/MainPane.js
@@ -1,17 +1,38 @@
 import SplitPane from "react-split-pane";
 import FilesPane from "./FilesPane";
 import { EditorPane } from "./EditorPane";
+import Button from 'react-bootstrap/Button';
+import runSimulation from "./hdlSimulator";
+import FileContext from "./FileContext";
+import { useContext } from "react";
+
+import SimulationContext from "./SimulationContext";
 
 export default function MainPane() {
-    return <SplitPane split="horizontal" minSize={50}
-        defaultSize={parseInt(localStorage.getItem('splitPosMainPane')||50)}
+    const { files, currFile, setCurrFile, setFiles } = useContext(FileContext);
+    const { setSimulationResult } = useContext(SimulationContext);
+    function deleteProject() {
+        setFiles([])
+        return localStorage.setItem('files', JSON.stringify([]));
+    }
+    return <SplitPane split="horizontal" minSize={30}
+        defaultSize={parseInt(localStorage.getItem('splitPosMainPane') || 30)}
         onChange={(size) => localStorage.setItem('splitPosMainPane', size)}>
-        <div>Top Pane -- Edited to test hosting</div>
+        {ButtonPane(files, currFile, setSimulationResult, deleteProject)}
         <SplitPane split="vertical"
             minSize={230}
-            defaultSize={parseInt(localStorage.getItem('splitPosBottomPane')||230)}
+            defaultSize={parseInt(localStorage.getItem('splitPosBottomPane') || 230)}
             onChange={(size) => localStorage.setItem('splitPosBottomPane', size)}>
             <FilesPane />
             <EditorPane />
         </SplitPane></SplitPane>;
 }
+function ButtonPane(files, currFile, setSimulationResult, deleteProject) {
+    return <div className="btn-group btn-block btn-group-xs buttonPane" role="group">
+        <Button className="btn-dark btn-sm" type="button" onClick={() => localStorage.setItem('files', JSON.stringify(files))}>Save</Button>
+        <Button className="btn-dark btn-sm" type="button" onClick={() => runSimulation(currFile, files, (result) => setSimulationResult(result))}>Run</Button>
+        <Button className="btn-dark btn-sm" type="button" href={`data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(files))}`} download="project.json">Download</Button>
+        <Button className="btn-dark btn-sm" type="button" onClick={() => deleteProject()}>Delete</Button>
+    </div>;
+}
+
