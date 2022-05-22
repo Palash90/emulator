bad9d757cd5d69530fd3f50685fcf4190644db86
Palash <palash.kanti.kundu.90@gmail.com>
2022-05-22 19:45:50 +0530

    Started calculating ast

--- src/parser/AstGenerator.js
+++ src/parser/AstGenerator.js
@@ -12,16 +12,58 @@ const AstGenerator = () => {
             var token = Peek();
             if (token.type === Token.EOF) {
                 break;
-            } else if (token.type === Token.CHIPDEF) {
+            } else if (token.type === Token.KEYWORD && token.value === 'CHIP') {
+                handleChipDefinition();
+            } else {
                 Consume();
-
-                ast.push({
-
-                });
             }
         }
 
         return ast;
+
+        function handleChipDefinition() {
+            Consume();
+
+            var chipName = Peek();
+            Consume();
+
+            if (chipName.type !== Token.CHIPDEF) {
+                throw new Error("Incorrect syntax at line " + chipName.line + " column " + chipName.column + ". Expected CHIP name, got " + chipName.value);
+            }
+
+            var l_brace = Peek();
+            Consume();
+            if (l_brace.type !== Token.OPERATOR || l_brace.value != '{') {
+                throw new Error("Incorrect syntax at line " + l_brace.line + " column " + l_brace.column + ". Expected '{', got " + l_brace.value);
+            }
+
+            var in_statement = Peek();
+            Consume();
+            if (in_statement.type !== Token.KEYWORD || in_statement.value !== 'IN') {
+                throw new Error("Incorrect syntax at line " + in_statement.line + " column " + in_statement.column + ". Expected 'IN', got " + in_statement.value);
+            }
+
+            var inputs = [];
+            while (true) {
+                var input_variable_token = Peek();
+                if (input_variable_token.type === Token.EOF) {
+                    throw new Error("Incorrect syntax at line " + in_statement.line + " column " + in_statement.column + ". Expected ';', got EOF");
+                }
+
+                if (input_variable_token.type === token.OPERATOR && input_variable_token.value === ';') {
+                    break;
+                }
+
+                Consume();
+            }
+
+            var node = {
+                name: chipName.value,
+                type: 'CHIP'
+            };
+
+            ast.push(node);
+        }
     }
 
 
