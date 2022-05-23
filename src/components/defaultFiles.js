89d953cdf5bbcd0c61818a60f24ec366d86f28e4
Palash <palash.kanti.kundu.90@gmail.com>
2022-05-23 16:56:26 +0530

    Saved 3 gates

--- src/components/defaultFiles.js
+++ src/components/defaultFiles.js
@@ -1,7 +1,19 @@
-const defaultFiles = [{
+const defaultFiles = [
+  {
+    "key": 2,
+    "name": "And.hdl",
+    "content": "/*\n* Applying De' Morgan's law.\n*/\nimport Not.hdl;\n\nCHIP And {\n  IN a, b;\n  OUT out;\n  \n  PARTS:\n    Not(a=a, out=notA);\n    Not(a=b, out=notB);\n    Nor(a=notA, b=notB, out=out);\n}"
+  },
+  {
     "key": 0,
-    "name": "Not",
+    "name": "Not.hdl",
     "content": "/*\n* Not CHIP\n*/\n\nCHIP Not {\n    IN a;\n    OUT out;\n\n    PARTS:\n     \n     Nor(a=a, b=a, out=out); // If we short two inputs of nor gate, we essentially get a not gate.                                                                                         \n}\n"
-}];
+  },
+  {
+    "key": 1,
+    "name": "Or.hdl",
+    "content": "/*\n* The Nor output needs to go through the Not gate to get Or gate\n*/\nimport Not.hdl;\n\nCHIP Or {\n  IN a, b;\n  OUT out;\n  \n  PARTS:\n    Nor(a=a, b=b, out=norOut);\n    Not(a=norOut, out=out);\n}"
+  }
+];
 
 export default defaultFiles;
