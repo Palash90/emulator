93188c84e6ef2dae004530a4dae8c6476d10ee0a
Palash <palash.kanti.kundu.90@gmail.com>
2022-05-24 20:42:10 +0530

    Multi Editor supported

--- src/components/EditorPane.js
+++ src/components/EditorPane.js
@@ -53,7 +53,7 @@ export const EditorPane = (props) => {
             minSize={400}
             defaultSize={parseInt(localStorage.getItem('splitPosEditorPane') || "400")}
             onChange={(size) => localStorage.setItem('splitPosEditorPane', size)}>
-            <MultipleEditors openFiles={openFiles} save={saveNewCode} />
+            <MultipleEditors openFiles={openFiles} currFile={currFile} save={saveNewCode} />
             <div className="editor">
                 <h6 className="output">Simulation Output</h6>
                 <OutputWindow />
@@ -64,31 +64,40 @@ export const EditorPane = (props) => {
 
 function MultipleEditors(props) {
     console.log(props);
-    if (props.openFiles && props.openFiles.length > 0)
-        return <div style={{ display: "flex", flexDirection: "row" }}>{
-            props.openFiles.map(element => {
-                console.log("Opening window for ", element);
-                return <EditorWindow key={element.key} file={element} save={props.save} />
-            })
-        }</div>
-    else
-        return <div>No file is open</div>
+    if (props.openFiles && props.openFiles.length > 0) {
+        return <div>
+            <div style={{ display: "flex", flexDirection: "row" }}>{
+                props.openFiles.map(element => {
+                    console.log("Opening window for ", element, props.currFile);
+                    return <div className="text-white file-header">{element.name}</div>
+                })
+            }</div>
+            <div style={{ position: "relative" }}>
+                {props.openFiles.map(element => {
+                    return <div style={{ margin: "5px", position: "absolute", zIndex: props.currFile === element.key ? 10 : 0 }}>
+                        <CodeMirror
+                            value={element.content}
+                            theme={oneDark}
+                            extensions={[javascript({ jsx: true })]}
+                            height="35vh"
+                            width="87vw"
+                            onChange={(value, viewUpdate) => {
+                                props.save(value);
+                            }}
+                        />
+                    </div>
+                })}
+            </div>
+        </div>
+    }
+    else {
+        return <></>
+    }
+
 }
 
 function EditorWindow(props) {
     console.log(props.file)
-    return <div style={{ margin: "5px" }}>
-        <p className="text-white">{props.file.name}</p>
-        <CodeMirror
-            value={props.file.content}
-            theme={oneDark}
-            extensions={[javascript({ jsx: true })]}
-            height="35vh"
-            width="87vw"
-            onChange={(value, viewUpdate) => {
-                props.save(value);
-            }}
-        />
-    </div>;
+    return;
 }
 
