714bb7c5e14fe0768bec41f4bc3cf7c865d35b7e
Palash <palash.kanti.kundu.90@gmail.com>
2022-05-27 12:59:26 +0530

    Test Input and Output variables

---
+++ src/test/ChipDefinitionTest/CHIP_DEF_IN_VAR_OUT.js
@@ -0,0 +1,5 @@
+const result = { error: true, errorMessage: "CHIP_DEF_IN_VAR_OUT.hdl:Expected Variable Definition, but got '}' at line:4 column:1" }
+
+module.exports = {
+    result
+}
---
+++ src/test/ChipDefinitionTest/CHIP_DEF_IN_VAR_R_BRACE.js
@@ -0,0 +1,5 @@
+const result = { error: true, errorMessage: "CHIP_DEF_IN_VAR_R_BRACE.hdl:Expected OUT, but got '}' at line:3 column:1" }
+
+module.exports = {
+    result
+}
