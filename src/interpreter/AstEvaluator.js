3c42160163c2862716d91e628f1aac2b9f80a455
Palash <palash.kanti.kundu.90@gmail.com>
2022-05-29 12:50:49 +0530

    Ast evaluation till import

--- src/interpreter/AstEvaluator.js
+++ src/interpreter/AstEvaluator.js
@@ -2,14 +2,45 @@ const Token = require("./Token");
 
 const evaluate = (ast) => {
 
-    return {
-        error: true,
-        ast: ast,
-        result: "eval",
-        errorMessage:"Bad happened"
+    var result = {};
+
+    if (ast.error) {
+        result.error = true;
+        result.result = {
+            ast: ast
+        };
+        result.errorMessage = "Could not evaluate due to parse error";
+        return result;
+    }
+
+    result.error = false;
+    result.result = {
+        evaluationResult: evaluateAst(ast.ast.file, ast.ast.chipDefinition, ast.ast.ast),
+        ast: ast
+    }
+
+    return result;
+}
+
+const evaluateAst = (fileName, chipName, ast) => {
+    console.log("Processing", fileName, chipName, ast)
+    var evaluationResult = {
+        fileName: fileName,
+        chip: chipName
     }
 
 
+    var importedChips = [];
+
+    var imports = ast.filter(el => el.type === Token.IMPORT)
+
+    console.log("imports", imports)
+
+    imports.map(el => importedChips.push(evaluateAst(el.importedAst.file, el.importedAst.chipDefinition, el.importedAst.ast)))
+
+    evaluationResult.importedChips = importedChips;
+
+    return evaluationResult;
 }
 
 module.exports = {
