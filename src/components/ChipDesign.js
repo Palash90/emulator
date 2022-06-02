e0df6e81536cf3fdaab9b609e9ac0ce119ed7bad
Palash <palash.kanti.kundu.90@gmail.com>
2022-06-02 12:20:49 +0530

    started with chip design component

---
+++ src/components/ChipDesign.js
@@ -0,0 +1,19 @@
+import uuid from "react-uuid";
+import { useContext } from "react";
+import { Container, Navbar, Table } from "react-bootstrap";
+import SimulationContext from "./SimulationContext";
+
+
+export default function ChipDesign() {
+    const { simulationResult } = useContext(SimulationContext);
+
+    console.log(simulationResult);
+
+    return <>
+        <div class="svg-container">
+            <svg viewBox="0 0 60 100">
+                <path fill="#ABABAB" d="M59.717,50.177c0-13.252-3.631-25.945-10.495-36.82l2.998-1.873L39.891,0.667l4.318,15.823l3.1-1.937 c6.64,10.515,10.152,22.797,10.152,35.624c0,12.927-3.56,25.284-10.294,35.848l-2.959-1.849L39.891,100L52.22,89.183l-3.14-1.962 C56.037,76.298,59.717,63.529,59.717,50.177z" />
+            </svg>
+        </div>
+    </>
+}
--- src/components/OutputWindow.js
+++ src/components/OutputWindow.js
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react";
 import SimulationContext from "./SimulationContext";
 import TruthTable from "./TruthTable";
+import ChipDesign from "./ChipDesign";
 
 function OutputWindow(props) {
     const { simulationResult } = useContext(SimulationContext);
@@ -8,8 +9,6 @@ function OutputWindow(props) {
     const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
 
     const [callStack, setCallStack] = useState();
-
-    console.log(simulationResult)
 
     if (simulationResult && Object.keys(simulationResult).length === 0 && Object.getPrototypeOf(simulationResult) === Object.prototype) {
         return <></>
@@ -19,7 +18,10 @@ function OutputWindow(props) {
         return <pre className="text-warning  border border-secondary" style={{ textAlign: 'left', wordWrap: 'break-word', whiteSpace: 'pre-wrap', overflowY: "scroll", width: (vw * 99 / 100 - props.editorWidth) + "px", marginTop: '10px', height: vh * 85 / 100 + "px" }} role="output">{simulationResult.errorMessage}</pre>
     } else {
 
-        return <TruthTable setCallStack={setCallStack} />;
+        return <>
+            <ChipDesign />
+            <TruthTable setCallStack={setCallStack} />
+        </>
     }
 }
 
