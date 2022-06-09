dd5ab3784a0d24cdb8967582a2019b6851988c89
Palash <palash.kanti.kundu.90@gmail.com>
2022-06-10 01:09:47 +0530

    Clock automatic

--- src/components/ClockModule.js
+++ src/components/ClockModule.js
@@ -1,26 +1,49 @@
-import { Form } from "react-bootstrap";
-import { useState } from "react";
+import { Col, Form, Row } from "react-bootstrap";
+import { useState, useEffect } from "react";
 
 export default function ClockModule(props) {
     const [manual, setManual] = useState();
+    const [freq, setFreq] = useState(100);
 
     var clock = () => {
         if (manual)
             props.setClockState(!props.clockState);
     }
 
+    useEffect(() => {
+        const interval = !manual ? setInterval(() => {
+            props.setClockState(!props.clockState);
+        }, freq) : null;
+        return () => clearInterval(interval);
+    }, [manual, freq, props]);
+
     var clockStyle = { border: "solid 1px " + (manual ? "white" : "gray"), height: "25", width: "40", cursor: manual ? "pointer" : "default" }
 
     return <div className="clock">
         <div>
+            Clock Controls
+        </div>
+        <div>
             <Form>
-                <Form.Check
-                    type="switch"
-                    id="custom-switch"
-                    label="Manual"
-                    value={manual}
-                    onChange={(value) => setManual(value.target.checked)}
-                />
+                <Form.Group sm={12}>
+                    <Row>
+                        <Col>
+                            <Form.Check
+                                type="switch"
+                                id="custom-switch"
+                                label="Manual"
+                                value={manual}
+                                onChange={(value) => setManual(value.target.checked)}
+                            />
+                        </Col>
+                        <Col>
+                            <Form.Range disabled={manual} value={freq} step={100} min={100} max={2500} onChange={(value) => setFreq(value.target.value)} />
+                        </Col>
+                        <Col>
+                            <Form.Label>{freq + " ms"}</Form.Label>
+                        </Col>
+                    </Row>
+                </Form.Group>
             </Form>
         </div>
         <div>
@@ -32,5 +55,5 @@ export default function ClockModule(props) {
                 <line x1="25" x2="35" y1="20" y2="20" stroke={manual ? "white" : "gray"} strokeWidth="2" />
             </svg>
         </div>
-    </div>
+    </div >
 }
